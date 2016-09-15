102c3fed936a2a35c08c459714440336247e2543
karamansky <karamansky@gmail.com>
2016-09-15 10:04:34 +0300

    + Блок Портфолио

--- js/common.js
+++ js/common.js
@@ -2,6 +2,28 @@
 
 $(function(){
 
-	//$(document).ready(alert("Test GIT"));
+	$(document).ready(function(){
 
+		$('#container').mixItUp({
+			animation: {
+				enable: true,
+				effects: 'fade scale',
+				duration: 600,
+				easing: 'ease',
+				perspectiveDistance: '3000px',
+				perspectiveOrigin: '50% 50%',
+				queue: true,
+				queueLimit: 1,
+				animateChangeLayout: false,
+				animateResizeContainer: true,
+				animateResizeTargets: false,
+				staggerSequence: null,
+				reverseOut: false
+			},
+			controls: {
+				activeClass: 'active'
+			}
+		});
+
+	});
 });
