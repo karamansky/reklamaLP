e1fa64219f550a58e33f34c5c9ab7cbfb8fdeb0c
karamansky <karamansky@gmail.com>
2016-09-16 08:49:20 +0300

    70% index.html

--- js/common.js
+++ js/common.js
@@ -4,6 +4,17 @@ $(function(){
 
 	$(document).ready(function(){
 
+	//плавающий header
+		var header = new Headhesive('header', {
+				offset: 500,
+				classes: {
+					clone: 'headhesive',
+					stick: 'headhesive--stick',
+					unstick: 'headhesive--unstick'
+				}
+		});
+
+	//portfolio gallery
 		$('#container').mixItUp({
 			animation: {
 				enable: true,
@@ -24,6 +35,20 @@ $(function(){
 				activeClass: 'active'
 			}
 		});
+
+	//owl-carousel
+		$('.owl-carousel').owlCarousel({
+			items 						: 5,
+			itemsDesktop 			: [1199,5],
+			itemsDesktopSmall : [980,3],
+			itemsTablet				: [768,2],
+			itemsMobile 			: [479,1],
+			singleItem 				: false,
+			itemClass					: ".item-owl",
+			responsive				: true,
+			autoPlay					: true
+		});
+
 
 	});
 });
